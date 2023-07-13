9d436195b51831e928d1483934ffb1d964469a48
Vincent Vrijburg <vincentvrijburg@gmail.com>
2023-07-13 21:27:34 +0200

    Remove unused test base field

--- tests/Moneybird.Net.Tests/Endpoints/Webhooks/WebhookEndpointTestBase.cs
+++ tests/Moneybird.Net.Tests/Endpoints/Webhooks/WebhookEndpointTestBase.cs
@@ -3,5 +3,4 @@ namespace Moneybird.Net.Tests.Endpoints.Webhooks;
 public class WebhookEndpointTestBase : CommonTestBase
 {
     protected static string WebhookId = "392975655159990043";
-    protected static string WebhookToken = "uYHZgRiZzeFxjNzP5f9nTN2E";
 }
