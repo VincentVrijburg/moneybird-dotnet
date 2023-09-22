4ea7bc6d49ea7e9fb623fa5a9dc71ab802fba66d
Vincent Vrijburg <vincentvrijburg@gmail.com>
2023-09-22 20:17:29 +0200

    Add support for the financial accounts endpoint

--- src/Moneybird.Net/Abstractions/IMoneybirdClient.cs
+++ src/Moneybird.Net/Abstractions/IMoneybirdClient.cs
@@ -31,6 +31,11 @@ namespace Moneybird.Net.Abstractions
         IExternalSalesInvoiceEndpoint ExternalSalesInvoice { get; }
         
         /// <summary>
+        /// The FinancialAccount Endpoint.
+        /// </summary>
+        IFinancialAccountEndpoint FinancialAccount { get; }
+        
+        /// <summary>
         /// The LedgerAccount Endpoint.
         /// </summary>
         ILedgerAccountEndpoint LedgerAccount { get; }
