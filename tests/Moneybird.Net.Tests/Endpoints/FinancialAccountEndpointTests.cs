29c80ee6bea1dafe69524610ad60c1bf1753b495
Vincent Vrijburg <vincentvrijburg@gmail.com>
2023-11-26 15:21:45 +0100

    Move endpoint tests

---
+++ tests/Moneybird.Net.Tests/Endpoints/FinancialAccountEndpointTests.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using FluentAssertions;
+using Moneybird.Net.Endpoints;
+using Moneybird.Net.Entities.FinancialAccounts;
+using Moneybird.Net.Http;
+using Moq;
+using Xunit;
+
+namespace Moneybird.Net.Tests.Endpoints;
+
+public class FinancialAccountEndpointTests : CommonTestBase
+{
+    private static Mock<IRequester> _requester;
+    private readonly MoneybirdConfig _config;
+    private readonly FinancialAccountEndpoint _financialAccountEndpoint;
+    
+    private const string GetFinancialAccountsResponsePath = "./Responses/Endpoints/FinancialAccounts/getFinancialAccounts.json";
+
+    public FinancialAccountEndpointTests()
+    {
+        _requester = new Mock<IRequester>();
+        _config = new MoneybirdConfig();
+        _financialAccountEndpoint = new FinancialAccountEndpoint(_config, _requester.Object);
+    }
+    
+    [Fact]
+    public async void GetFinancialAccountsAsync_ByAccessToken_Returns_FinancialAccounts()
+    {
+        var financialAccountsList = await File.ReadAllTextAsync(GetFinancialAccountsResponsePath);
+
+        _requester.Setup(moq => moq.CreateGetRequestAsync(It.IsAny<string>(), It.IsAny<string>(),
+            It.IsAny<string>(), It.IsAny<List<string>>())).ReturnsAsync(financialAccountsList);
+
+        var financialAccounts = JsonSerializer.Deserialize<List<FinancialAccount>>(financialAccountsList, _config.SerializerOptions);
+        Assert.NotNull(financialAccounts);
+
+        var actualFinancialAccounts = await _financialAccountEndpoint.GetAsync(AdministrationId, AccessToken);
+        Assert.NotNull(actualFinancialAccounts);
+        
+        var actualFinancialAccountsList = actualFinancialAccounts.ToList();
+        Assert.Equal(financialAccounts.Count, actualFinancialAccountsList.Count);
+        
+        foreach (var actualFinancialAccount in actualFinancialAccountsList)
+        {
+            var financialAccount = financialAccounts.FirstOrDefault(fa => fa.Id == actualFinancialAccount.Id);
+            Assert.NotNull(financialAccount);
+
+            financialAccount.Should().BeEquivalentTo(actualFinancialAccount);
+        }
+    }
+}
--- tests/Moneybird.Net.Tests/Endpoints/LedgerAccounts/LedgerAccountTestBase.cs
+++
@@ -1,6 +0,0 @@
-namespace Moneybird.Net.Tests.Endpoints.LedgerAccounts;
-
-public class LedgerAccountTestBase : CommonTestBase
-{
-    protected static string LedgerAccountId = "387348630529377910";
-}
