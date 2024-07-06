7c004d7160cd063016e297b70fe174c1328ccbe2
Vincent Vrijburg <vincentvrijburg@gmail.com>
2024-07-06 20:20:44 +0200

    Exclude requester base from coverage

--- src/Moneybird.Net/Http/RequesterBase.cs
+++ src/Moneybird.Net/Http/RequesterBase.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -10,6 +11,7 @@ using Moneybird.Net.Extensions;
 
 namespace Moneybird.Net.Http
 {
+    [ExcludeFromCodeCoverage]
     public abstract class RequesterBase : IDisposable
     {
         private readonly HttpClient _httpClient;
